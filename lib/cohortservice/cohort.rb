d19ec266d27abe7f26a8426587c576d980274410
jejacks0n <jejacks0n@gmail.com>
2016-10-26 14:56:12 -0600

    Adds some refinements and adjusts the public api / data source

---
+++ lib/cohortservice/cohort.rb
@@ -0,0 +1,59 @@
+class Cohortservice::Cohort
+  class Parts
+    attr_reader :year, :month, :product, :campus
+
+    def initialize(parts_array)
+      @year = parts_array[0].to_i
+      @sequence = parts_array[1]
+      @product = parts_array[2]
+      @campus = parts_array[3]
+    end
+
+    def valid?
+      return false if year < 11 || year > 30
+      return false if Cohortservice.data['products'][product].nil?
+      return false if Cohortservice.data['campuses'][campus].nil?
+      true
+    end
+  end
+
+  def initialize(label)
+    @label = label
+    @parts = Parts.new(label.to_s.split('-'))
+    raise Cohortservice::ValidationError unless @parts.valid?
+  end
+
+  def info
+    @info ||= {
+      course: Cohortservice.data['products'][@parts.product]['course'],
+      education: Cohortservice.data['products'][@parts.product]['education'],
+      campus: Cohortservice.data['campuses'][@parts.campus]['name'],
+      label: @label,
+      location: Cohortservice.data['campuses'][@parts.campus]['location']
+    }
+  end
+
+  def course
+    info[:course]
+  end
+
+  def education
+    info[:education]
+  end
+
+  def campus
+    info[:campus]
+  end
+
+  def location
+    info[:location]
+  end
+
+  def latlng
+    {
+      lat: info[:location][:lat],
+      lng: info[:location][:lng],
+    }
+  end
+
+end
